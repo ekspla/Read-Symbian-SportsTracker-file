f9988a28d2acbf36381b4342b4075ff19e8a87e3
ekspla <ekspla@sky.plala.or.jp>
2021-02-11 18:12:53 +0900

    Changes in style.

--- convert_nst_files_to_gpx.py
+++ convert_nst_files_to_gpx.py
@@ -30,46 +30,48 @@ def args_usage():
     #print(in_file)
     return in_file
 
-def check_file_type_version(file_obj):
+def check_file_type_version(f):
     # Check if it is the correct file.
-    #file_obj.seek(0x00000, 0)
+    #f.seek(0x00000, 0)
     # 8 (4+4) bytes, little endian U32+U32.
-    (application_id, nst.FILE_TYPE) = nst.read_unpack('<2I', file_obj)
+    (application_id, nst.FILE_TYPE) = nst.read_unpack('<2I', f)
     if application_id != nst.APP_ID or nst.FILE_TYPE not in {TRACK, ROUTE}:
         print(f'Unexpected file type: {nst.FILE_TYPE}')
         sys.exit(1)
 
     # Preliminary version check.
-    #file_obj.seek(0x00008, 0) # Go to 0x00008, this address is fixed.
-    (version, ) = nst.read_unpack('<I', file_obj) # 4 bytes, little endian U32.
+    #f.seek(0x00008, 0) # Go to 0x00008, this address is fixed.
+    (version, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
     #print(f'Version: {version}')
     (nst.OLDNST, nst.OLDNST_ROUTE, nst.NST) = (
         version < 10000, 10000 <= version < 20000, 20000 <= version)
 
-def parse_info_part_of_tracks(file_obj):
+def parse_info_part_of_tracks(f):
     # Track ID and Totaltime.
     track_id_addr = 0x00014 # Fixed addresses of oldNST and the new NST tracks.
     if nst.FILE_TYPE == TMP: track_id_addr += 0x04 # The 4-byte blank (0x18).
-    file_obj.seek(track_id_addr, 0) # 8 (4+4) bytes, little endian U32+U32.
-    (TRACK_ID, total_time) = nst.read_unpack('<2I', file_obj)
-    #print(f'Track ID: {TRACK_ID}')
+    f.seek(track_id_addr, 0) # 8 (4+4) bytes, little endian U32+U32.
+    (track_id, total_time) = nst.read_unpack('<2I', f)
+    del track_id # Not in use.
+    #print(f'Track ID: {track_id}')
 
     nst.total_time = total_time / 100 # Totaltime in seconds.
     #print(f'Total time: {nst.format_timedelta(nst.total_time)}')
 
     # Total Distance.
-    if nst.NST: file_obj.seek(0x00004, 1) # Skip.  4-byte offset to oldNST due to this.
-    (total_distance, ) = nst.read_unpack('<I', file_obj) # 4 bytes, little endian U32.
+    if nst.NST: f.seek(0x00004, 1) # Skip.  4-byte offset to OLDNST.
+    (total_distance, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
     nst.total_distance = total_distance / 1e5 # Total distance in km.
     #print(f'Total distance: {round(nst.total_distance, 3)} km')
 
     # Calculate Net speed in km/h.
     net_speed = nst.total_distance / (nst.total_time / 3600) # km/h
+    del net_speed # Not in use.
     #print(f'Net speed: {round(net_speed, 3)} km/h')
 
     # Starttime and Stoptime in localtime.
     # 16 (8+8) bytes, little endian I64+I64.
-    (start_localtime, stop_localtime) = nst.read_unpack('<2q', file_obj)
+    (start_localtime, stop_localtime) = nst.read_unpack('<2q', f)
     nst.START_LOCALTIME = nst.symbian_to_unix_time(start_localtime)
     nst.stop_localtime = nst.symbian_to_unix_time(stop_localtime)
 
@@ -85,15 +87,16 @@ def parse_info_part_of_tracks(file_obj):
 
     # Calculate Gross speed in km/h.
     gross_speed = nst.total_distance / (real_time / 3600) # km/h
+    del gross_speed # Not in use.
     #print(f'Gross speed: {round(gross_speed, 3)} km/h')
 
     # User ID, please see config.dat.
-    (nst.USER_ID, ) = nst.read_unpack('<I', file_obj) # 4 bytes, little endian U32.
+    (nst.USER_ID, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
     #print(f'User id: {nst.USER_ID}')
 
     # Type of activity.  Walk, run, bicycle, etc. See config.dat for details.
-    file_obj.seek(0x00004, 1) # Skip 4 bytes.
-    (activity, ) = nst.read_unpack('<H', file_obj) # 2 bytes, little endian U16.
+    f.seek(0x00004, 1) # Skip 4 bytes.
+    (activity, ) = nst.read_unpack('<H', f) # 2 bytes, little endian U16.
     nst.activity_type = (str(activity) if activity >= len(nst.ACTIVITIES) 
                          else nst.ACTIVITIES[activity])
     #print(f'Activity: {nst.activity_type}')
@@ -105,15 +108,15 @@ def parse_info_part_of_tracks(file_obj):
     track_name_addr = 0x00046 # This is the fixed address of the oldNST track.
     if nst.NST: track_name_addr += 0x04 # Offset at total_distance (-> 0x4a).
     if nst.FILE_TYPE == TMP: track_name_addr += 0x04 # 4-byte blank (-> 0x4e).
-    nst.track_name = nst.scsu_reader(file_obj, track_name_addr)
+    nst.track_name = nst.scsu_reader(f, track_name_addr)
     #print(f'Track name: {nst.track_name}')
 
     # Starttime & Stoptime in UTC.
     start_stop_z_addr = 0x0018e # This is the fixed address of oldNST track.
     if nst.NST: start_stop_z_addr += 0x04 # Offset at total_distance (0x192).
     if nst.FILE_TYPE == TMP: start_stop_z_addr += 0x04 # 4-byte blank (0x196).
-    file_obj.seek(start_stop_z_addr, 0) # 16 (8+8) bytes, little endian I64+I64.
-    (start_time, stop_time) = nst.read_unpack('<2q', file_obj)
+    f.seek(start_stop_z_addr, 0) # 16 (8+8) bytes, little endian I64+I64.
+    (start_time, stop_time) = nst.read_unpack('<2q', f)
     nst.START_TIME = nst.symbian_to_unix_time(start_time)
     nst.stop_time = nst.symbian_to_unix_time(stop_time)
     #print(f'Start Z: {nst.format_datetime(nst.START_TIME)}Z')
@@ -130,38 +133,41 @@ def parse_info_part_of_tracks(file_obj):
         # Read SCSU encoded user comment of variable length.
         comment_addr = 0x00222 # Fixed address of NST tracks.
         if nst.FILE_TYPE == TMP: comment_addr += 0x4 # The 4-byte blank (0x226).
-        nst.comment = nst.scsu_reader(file_obj, comment_addr) # This address is fixed.
+        nst.comment = nst.scsu_reader(f, comment_addr) # This address is fixed.
         #if nst.comment: print(f'Comment: {nst.comment}')
 
-def parse_info_part_of_routes(file_obj):
+def parse_info_part_of_routes(f):
     # Route ID.
-    file_obj.seek(0x00014, 0) # Go to 0x00014, this address is fixed.
-    (ROUTE_ID, ) = nst.read_unpack('<I', file_obj) # 4 bytes, little endian U32.
-    #print(f'Route ID: {ROUTE_ID}')
+    f.seek(0x00014, 0) # Go to 0x00014, this address is fixed.
+    (route_id, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
+    del route_id # Not in use.
+    #print(f'Route ID: {route_id}')
 
     # Read SCSU encoded name of the route.  Its length is variable.
-    #file_obj.seek(0x00018, 0) # Go to 0x00018, this address is fixed.
-    nst.route_name = nst.scsu_reader(file_obj)
+    #f.seek(0x00018, 0) # Go to 0x00018, this address is fixed.
+    nst.route_name = nst.scsu_reader(f)
     #print(f'Route name: {nst.route_name}')
 
     # Totaltime is not stored in the route file.
 
     # Total Distance.
-    (total_distance, ) = nst.read_unpack('<I', file_obj) # 4 bytes, little endian U32.
+    (total_distance, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
     nst.total_distance = total_distance / 1e5 # Total distance in km.
     #print(f'Total distance: {round(nst.total_distance, 3)} km')
 
-def read_pause_and_track(file_obj, start_address):
-    file_obj.seek(start_address, 0) # Go to the start address of the main part.
+def read_pause_and_track(f, start_address):
+    f.seek(start_address, 0) # Go to the start address of the main part.
     # Read pause data.  There is no pause data in route file.
     (pause_list, pause_count) = ( # Do not read pause data if ROUTE or TMP.
         ([], None) if nst.FILE_TYPE in {ROUTE, TMP} 
-        else nst.read_pause_data(file_obj))
-    #if nst.FILE_TYPE == TRACK: nst.print_pause_list(pause_list) # For debugging purposes.
+        else nst.read_pause_data(f))
+    del pause_count # Not in use.
+    #if nst.FILE_TYPE == TRACK: nst.print_pause_list(pause_list) # For debug.
     #sys.exit(0)
 
     # Read trackpoint data.  The last trackpt_store is necessary in summarizing.
-    (track_count, trackpt_store) = nst.read_trackpoints(file_obj, pause_list)
+    (track_count, trackpt_store) = nst.read_trackpoints(f, pause_list)
+    del track_count # Not in use.
     return trackpt_store
 
 WRITE_FILE = False
@@ -173,16 +179,16 @@ def main():
         check_file_type_version(f)
         gpx, nst.gpx_target = nst.initialize_gpx()
 
-        # Start address of the main part (a pause data block and a trackpt block).
+        # Start address of the main part (a pause data and a trackpt block).
         #f.seek(0x0000C, 0) # Go to 0x0000C, this address is fixed.
-        # Usually the numbers are for 
+        # Usually the numbers (4 bytes, little endian U32) are for 
         #     the new track 0x0800 = 0x07ff + 0x1, 
         #     the old track 0x0400 = 0x03ff + 0x1 and 
         #     the old route 0x0100 = 0x00ff + 0x1
         # but can be changed in a very rare case.
-        (START_ADDRESS, ) = nst.read_unpack('<I', f) # 4 bytes, little endian U32.
-        START_ADDRESS -= 1
-        #print(f'Main part address: {hex(START_ADDRESS)}')
+        (start_address, ) = nst.read_unpack('<I', f)
+        start_address -= 1
+        #print(f'Main part address: {hex(start_address)}')
 
         # Read information part of track/route files.
         if nst.FILE_TYPE == TRACK:
@@ -190,8 +196,8 @@ def main():
         elif nst.FILE_TYPE == ROUTE:
             parse_info_part_of_routes(f)
 
-        # Read the main part consisting of a pause- and a trackpoint-data blocks.
-        trackpt_store = read_pause_and_track(f, START_ADDRESS)
+        # Read the main part consisting a pause- and a trackpoint-data blocks.
+        trackpt_store = read_pause_and_track(f, start_address)
 
     nst.add_gpx_summary(gpx, trackpt_store)
 
