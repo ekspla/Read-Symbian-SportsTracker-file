896653f5975173cc684ac800075cacae7956617e
ekspla <ekspla@sky.plala.or.jp>
2021-03-11 12:41:38 +0900

    Added keywords.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ekspla/Read-Symbian-SportsTracker-file',
+    keywords='sports-tracker symbian nokia gps gpx',
     packages=find_packages(),
     include_package_data=True,
     license='LGPL',
