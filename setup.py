22002a72e706cd03499e3a4ce89d2168374a1c2b
ekspla <ekspla@sky.plala.or.jp>
2021-04-09 08:52:59 +0900

    Version += 0.0.1, for xml_declaration fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='symbian_sports_tracker',
-    version='0.9.6',
+    version='0.9.7',
     author='ekspla',
     description='Read-Symbian-SportsTracker-file',
     long_description=long_description,
